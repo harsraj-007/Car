384b73da666569d5aaa502a44c398bfd02fcedf1
harsraj-007 <140935578+harsraj-007@users.noreply.github.com>
2024-07-26 13:59:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.querySelector(".car").addEventListener("click",
+    function (){
+        var audio = new Audio("sound.mp3");
+        audio.loop=true;
+        audio.play();
+        
+    }
+    );
+
